450f4720d09ba81cf753e6a2dd13d1d0eb331ca1
Devin Barry <dbar061@aucklanduni.ac.nz>
2012-10-26 09:31:33 +1300

    uodates

--- FruitProcessingPlant/src/controller/ReceiveServer.java
+++ FruitProcessingPlant/src/controller/ReceiveServer.java
@@ -17,24 +17,7 @@ public class ReceiveServer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ServerQueue<Inventory> bq = new ServerQueue<Inventory>();
-		//Start server
-		System.out.println("Starting Main Server");
-		
-		//StringReceiveServer srs1 = new StringReceiveServer("55552");
-		//Thread t1 = new Thread(srs1);
-		
-		//These call are not super elegant but they get the job done.
-		ObjectReceiveServer<Inventory> ors1 = ObjectReceiveServer.createMyObject("55551", bq, Inventory.class);
-		
-		Thread t2 = new Thread(ors1);
-		t2.start();
-		
-		for (;;) {
-			Inventory nextB = bq.get();
-			System.out.println("We got Inventory: " + nextB);
-		}
-
+		testBlocking();
 	}
 	
 	private static void testDevinsStuff() {
@@ -61,6 +44,47 @@ public class ReceiveServer {
 			System.out.println("We got a boolean of " + nextB);
 			Integer nextI = iq.get();
 			System.out.println("We got an Integer of " + nextI);
+		}
+	}
+	
+	private static void testBlocking() {
+		ServerQueue<Boolean> bq = new ServerQueue<Boolean>();
+		//Start server
+		System.out.println("Starting Main Server");
+		
+		//StringReceiveServer srs1 = new StringReceiveServer("55552");
+		//Thread t1 = new Thread(srs1);
+		
+		//These call are not super elegant but they get the job done.
+		ObjectReceiveServer<Boolean> ors1 = ObjectReceiveServer.createMyObject("55555", bq, Boolean.class);
+		
+		Thread t2 = new Thread(ors1);
+		//t1.start();
+		t2.start();
+		
+		for (int i = 0; i < 5; i++) {
+			Boolean nextB = bq.get();
+			System.out.println("We got a boolean of " + nextB);
+		}
+	}
+	
+	private static void testGetInventory() {
+		ServerQueue<Inventory> bq = new ServerQueue<Inventory>();
+		//Start server
+		System.out.println("Starting Main Server");
+		
+		//StringReceiveServer srs1 = new StringReceiveServer("55552");
+		//Thread t1 = new Thread(srs1);
+		
+		//These call are not super elegant but they get the job done.
+		ObjectReceiveServer<Inventory> ors1 = ObjectReceiveServer.createMyObject("55551", bq, Inventory.class);
+		
+		Thread t2 = new Thread(ors1);
+		t2.start();
+		
+		for (;;) {
+			Inventory nextB = bq.get();
+			System.out.println("We got Inventory: " + nextB);
 		}
 	}
 
